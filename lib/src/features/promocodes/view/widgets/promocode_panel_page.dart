a9adfa3bafe16c5712553644dd2dfdf920d1cf04
Roman <rkukoyev@mail.ru>
2023-07-12 15:02:39 +0600

    editing main bottomSheet

---
+++ lib/src/features/promocodes/view/widgets/promocode_panel_page.dart
@@ -0,0 +1,166 @@
+import 'package:flutter/material.dart';
+import 'package:shebalin/src/features/promocodes/view/widgets/promocode_screen.dart';
+import 'package:sliding_up_panel/sliding_up_panel.dart';
+
+import '../../../../theme/images.dart';
+import '../../../../theme/theme.dart';
+import 'input_promocode_panel_page.dart';
+
+class PromocodePanelPage extends StatefulWidget {
+  const PromocodePanelPage({super.key});
+
+  @override
+  State<PromocodePanelPage> createState() => _PromocodePanelPageState();
+}
+
+class _PromocodePanelPageState extends State<PromocodePanelPage> {
+  final PanelController _panelController = PanelController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(0, 80, 0, 0),
+      child: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            SizedBox(
+              height: MediaQuery.of(context).size.height,
+              child: SlidingUpPanel(
+                borderRadius: panelRadius,
+                backdropEnabled: true,
+                parallaxEnabled: true,
+                parallaxOffset: 0.05,
+                controller: _panelController,
+                defaultPanelState: PanelState.CLOSED,
+                minHeight: 0,
+                maxHeight: MediaQuery.of(context).size.height * 0.6,
+                header: Container(
+                  margin: const EdgeInsets.fromLTRB(176, 20, 0, 0),
+                  decoration: BoxDecoration(
+                    color: Colors.black.withOpacity(0.12),
+                    borderRadius: containerRadius,
+                  ),
+                  child: const SizedBox(
+                    height: 3,
+                    width: 50,
+                  ),
+                ),
+                panel: SingleChildScrollView(
+                  child:  InputPromocodePanelPage(),
+                ),
+                body: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Column(
+                      children: [
+                        Center(
+                          child: Image.asset(ImagesSources.ticketOutline),
+                        ),
+                        const SizedBox(
+                          height: 24,
+                        ),
+                        Text(
+                          'У вас еще нет билетов',
+                          style: Theme.of(context)
+                              .textTheme
+                              .bodySmall
+                              ?.copyWith(color: Colors.black.withOpacity(0.3)),
+                          textAlign: TextAlign.center,
+                        ),
+                      ],
+                    ),
+                    Align(
+                      alignment: Alignment.bottomCenter,
+                      child: Padding(
+                        padding: const EdgeInsets.only(bottom: 32),
+                        child: ElevatedButton(
+                          onPressed: (() => _panelController.open()),
+                          style: ButtonStyle(
+                            backgroundColor:
+                                MaterialStateProperty.all(accentTextColor),
+                            elevation: MaterialStateProperty.all(0),
+                            shape: MaterialStateProperty.all(
+                              RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(12),
+                              ),
+                            ),
+                            minimumSize: MaterialStateProperty.all(
+                              const Size(328, 48),
+                            ),
+                          ),
+                          child: Text(
+                            'Активировать промокод',
+                            style: Theme.of(context)
+                                .textTheme
+                                .bodySmall
+                                ?.copyWith(color: Colors.white),
+                          ),
+                        ),
+                      ),
+                    ),
+                    
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+// Column(
+//                   mainAxisAlignment: MainAxisAlignment.start,
+//                   crossAxisAlignment: CrossAxisAlignment.center,
+//                   children: [
+//                     const SizedBox(
+//                       width: 24,
+//                     ),
+//                     Column(
+//                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//                       children: [
+//                         Image.asset(ImagesSources.ticketOutline),
+//                         const SizedBox(
+//                           height: 24,
+//                         ),
+//                         Text(
+//                           'У вас еще нет билетов',
+//                           style: Theme.of(context)
+//                               .textTheme
+//                               .bodySmall
+//                               ?.copyWith(color: Colors.black.withOpacity(0.3)),
+//                           textAlign: TextAlign.center,
+//                         ),
+//                         ElevatedButton(
+//                           onPressed: (() => _panelController.open()),
+//                           style: ButtonStyle(
+//                             backgroundColor:
+//                                 MaterialStateProperty.all(accentTextColor),
+//                             elevation: MaterialStateProperty.all(0),
+//                             shape: MaterialStateProperty.all(
+//                               RoundedRectangleBorder(
+//                                 borderRadius: BorderRadius.circular(12),
+//                               ),
+//                             ),
+//                             minimumSize: MaterialStateProperty.all(
+//                               const Size(328, 48),
+//                             ),
+//                           ),
+//                           child: Text(
+//                             'Активировать промокод',
+//                             style: Theme.of(context)
+//                                 .textTheme
+//                                 .bodySmall
+//                                 ?.copyWith(color: Colors.white),
+//                           ),
+//                         ),
+//                       ],
+//                     ),
+//                     const SizedBox(
+//                       width: 5,
+//                     )
+//                   ],
+//                 ),
--- lib/src/features/promocodes/view/widgets/promocode_screen.dart
+++ lib/src/features/promocodes/view/widgets/promocode_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shebalin/src/features/user/view/widgets/ticket.dart';
 import 'package:shebalin/src/theme/images.dart';
 import 'package:shebalin/src/theme/theme.dart';
 
@@ -16,102 +17,79 @@ class _PromocodeScreenState extends State<PromocodeScreen> {
   final PanelController _panelController = PanelController();
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: null,
-      body: SlidingUpPanel(
-        controller: _panelController,
-        defaultPanelState: PanelState.CLOSED,
-        minHeight: 0,
-        maxHeight: MediaQuery.of(context).size.height * 0.3,
-        header: Container(
-          margin: const EdgeInsets.fromLTRB(176, 20, 0, 0),
-          decoration: BoxDecoration(
-            color: Colors.black.withOpacity(0.12),
-            borderRadius: containerRadius,
-          ),
-          child: const SizedBox(
-            height: 3,
-            width: 50,
-          ),
+    return SlidingUpPanel(
+      borderRadius: panelRadius,
+      backdropEnabled: true,
+      parallaxEnabled: true,
+      parallaxOffset: 0.05,
+      controller: _panelController,
+      defaultPanelState: PanelState.CLOSED,
+      minHeight: 0,
+      maxHeight: MediaQuery.of(context).size.height * 0.3,
+      header: Container(
+        margin: const EdgeInsets.fromLTRB(176, 20, 0, 0),
+        decoration: BoxDecoration(
+          color: Colors.black.withOpacity(0.12),
+          borderRadius: containerRadius,
         ),
-        panel: InputPromocodePanelPage(),
-        body: Center(
-          child: Scaffold(
-            body: SafeArea(
-              child: Center(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        child: const SizedBox(
+          height: 3,
+          width: 50,
+        ),
+      ),
+      panel: InputPromocodePanelPage(),
+      body: Center(
+        child: Column(
+          // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            const SizedBox(
+              width: 24,
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
                   children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: [
-                        IconButton(
-                          onPressed: () => Navigator.of(context).pop(),
-                          iconSize: 34,
-                          icon: Image.asset(ImagesSources.close),
-                          splashRadius: 24,
-                        ),
-                        const SizedBox(
-                          width: 24,
-                        )
-                      ],
-                    ),
-                    Column(
-                      children: [
-                        Image.asset(ImagesSources.ticketOutline),
-                        const SizedBox(
-                          height: 24,
-                        ),
-                        Text(
-                          'К сожалению, у вас еще нет \n активных промокодов',
-                          style: Theme.of(context)
-                              .textTheme
-                              .bodySmall
-                              ?.copyWith(color: Colors.black.withOpacity(0.3)),
-                          textAlign: TextAlign.center,
-                        ),
-                      ],
-                    ),
-                    Column(
-                      children: [
-                        ElevatedButton(
-                          onPressed: (() => _panelController.open()),
-                          style: ButtonStyle(
-                            backgroundColor:
-                                MaterialStateProperty.all(accentTextColor),
-                            elevation: MaterialStateProperty.all(0),
-                            shape: MaterialStateProperty.all(
-                              RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(12),
-                              ),
-                            ),
-                            minimumSize: MaterialStateProperty.all(
-                              const Size(328, 48),
-                            ),
-                          ),
-                          child: Text(
-                            'Активировать промокод',
-                            style: Theme.of(context)
-                                .textTheme
-                                .bodySmall
-                                ?.copyWith(color: Colors.white),
-                          ),
-                        ),
-                        const SizedBox(
-                          height: 50,
-                        )
-                      ],
-                    ),
+                    Image.asset(ImagesSources.ticketOutline),
+                const SizedBox(
+                  height: 24,
+                ),
+                Text(
+                  'У вас еще нет билетов',
+                  style: Theme.of(context)
+                      .textTheme
+                      .bodySmall
+                      ?.copyWith(color: Colors.black.withOpacity(0.3)),
+                  textAlign: TextAlign.center,
+                ),
                   ],
                 ),
-              ),
+                ElevatedButton(
+                  onPressed: (() => _panelController.open()),
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(accentTextColor),
+                    elevation: MaterialStateProperty.all(0),
+                    shape: MaterialStateProperty.all(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                    ),
+                    minimumSize: MaterialStateProperty.all(
+                      const Size(328, 48),
+                    ),
+                  ),
+                  child: Text(
+                    'Активировать промокод',
+                    style: Theme.of(context)
+                        .textTheme
+                        .bodySmall
+                        ?.copyWith(color: Colors.white),
+                  ),
+                ),
+              ],
             ),
-          ),
+          ],
         ),
-        borderRadius: panelRadius,
-        backdropEnabled: true,
-        parallaxEnabled: true,
-        parallaxOffset: 0.05,
       ),
     );
   }
